8b539b3b308354036f4ec5977be773f5475e7c25
iivanov21 <iivanov21@hotmail.com>
2021-07-08 15:32:42 +0000

    II:Exmaple finished

--- programs/example.py
+++ programs/example.py
@@ -14,5 +14,13 @@
     # What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
 def endsPy(input):
-    return True
+    if input.endswith("py") or input.endswith("pY") or input.endswith("Py") or input.endswith("PY"):
+        return True
+    else:
+        return False
     
+if __name__ == "__main__":
+    print(endsPy("ilovepy") )
+    print(endsPy("welovepY") )
+    print(endsPy("welovepyforreal") )
+    print(endsPy("pyiscool") )
